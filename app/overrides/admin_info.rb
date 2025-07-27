255a31a69158b41032ef7456db0641847c99f93e
arean82 <arean82@gmail.com>
2025-07-27 22:02:15 +0530

    updaeted additionals

--- app/overrides/admin_info.rb
+++ app/overrides/admin_info.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
+#module AdminInfo
+#  Deface::Override.new virtual_path: 'admin/info',
+#                       name: 'add-system_info',
+#                       insert_after: 'table.list',
+#                       original: '3fa222a7a7d371fd24314cd0c3fc29b490139cf9',
+#                       partial: 'admin/system_info'
+#end
+
+
+# frozen_string_literal: true
+
 module AdminInfo
   Deface::Override.new virtual_path: 'admin/info',
                        name: 'add-system_info',
                        insert_after: 'table.list',
-                       original: '3fa222a7a7d371fd24314cd0c3fc29b490139cf9',
                        partial: 'admin/system_info'
 end
